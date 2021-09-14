53abc72182b76bede1de760d5235ff690ed72589
nataliephillips <nwp1068@gmail.com>
2021-09-14 14:36:53 -0700

    index

---
+++ python/lesson.py
@@ -0,0 +1,27 @@
+print('Hello world!')
+print('What is your name?') # ask for their name
+myName = input()
+print ('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?') # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge)+1) + ' in a year')
+#functions : print function for making text appears on the scream, there is an input function, and a changing function that can convert ints to floats to strings
+#boolena vlaues, datatype has two values of true and false
+#comparison operators ==, !=, <, >, <=, >= } these help with boolean values
+42 == 42
+myAge = 26
+#myAge < 30 is true
+#three boolean operators : and, or and not
+# true and true is true, true and false is false, false and false is false
+# true or false is true, false or false is false
+# not operator: not true is false, and not false is true
+# float control statements: if or and else
+name = 'alice'
+if name == 'alice': #this is the condition statement in the expression
+    print ('hi alice') #if the condition is true then the function will execute this
+print ('done')
+
+
+
